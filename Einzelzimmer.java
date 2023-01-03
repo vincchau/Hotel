8daf6e2f96f1481f3a9d5ac0208b26b336404b44
H1ght0w9r <120497846+H1ght0w9r@users.noreply.github.com>
2023-01-03 21:28:16 +0100

    Update Einzelzimmer.java

--- Einzelzimmer.java
+++ Einzelzimmer.java
@@ -7,20 +7,25 @@ public class Einzelzimmer extends Zimmer{
         super(zimmernummer, preiskategorie, preisProZimmer, zimmerProKategorie, belegt);
         this.balkon = balkon;
     }//end constructor Einzelzimmer()
+    //erzeugt ein Objekt des Typen Einzelzimmers und fügt das Attribut Balkon hinzu
     public Einzelzimmer(){
         super();
         this.balkon = false;
     }//end constructor Einzelzimmer()
+    //setzt den PreisproZimmer auf 50 & und die Kategorie auf 1
     public Einzelzimmer(int zimmernummer, boolean balkon){
         super(zimmernummer, 1, 50.00);
         this.balkon = balkon;
-    }
+    }//end constructor Einzelzimmer()
+    //macht das gleiche wie der vorangegangene nur für den Fall des Einzelzimmerzuschlags,
+    //indem er das Attribut einzelzimmerzuschlag hinzufügt, er passt ebenfalls die Preise an
     public Einzelzimmer(int zimmernummer, boolean balkon, boolean einzelzimmerzuschlag){
         super(zimmernummer, 2, 75.00);
         this.balkon = balkon;
     }
     //
     //Methoden
+    //ruft die Zimmer print Methode auf, mit der besonderheit, dass hier auch noch der Balkon, falls er gebucht wird mit ausgegeben wird
     public void print(){
         super.print();
         if(balkon){
