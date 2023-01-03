11a766fdf05e2cb6a410041b280097adbbe2ad71
H1ght0w9r <120497846+H1ght0w9r@users.noreply.github.com>
2023-01-03 21:29:12 +0100

    Update Tripplezimmer.java

--- Tripplezimmer.java
+++ Tripplezimmer.java
@@ -2,6 +2,7 @@ public class Tripplezimmer extends Zimmer{
     //Attribute
     //
     //Konstruktoren
+    //erzeugt ein Objekt des Typen Tripplezimmer mit den für ihn vorgesehenen fixen Werten
     public Tripplezimmer(int zimmernummer){
         super(zimmernummer, 3, 100.00);
     }//end constructor Tripplezimmer
@@ -9,6 +10,7 @@ public class Tripplezimmer extends Zimmer{
         super();
     }//end constructor Tripplezimmer
     //Methoden
+    //ruft die Zimmer print Methode auf
     public void print(){
         super.print();
         System.out.println("");
