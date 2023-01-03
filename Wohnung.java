7b1f4d19e6a3f2f33084ce6cc9446fe5d0b603e3
H1ght0w9r <120497846+H1ght0w9r@users.noreply.github.com>
2023-01-03 21:29:43 +0100

    Update Wohnung.java

--- Wohnung.java
+++ Wohnung.java
@@ -2,11 +2,13 @@ public class Wohnung extends Zimmer{
     //Attribute
     //
     //Konstruktoren
+    //erzeugt ein Objekt des Typen Wohnung mit den für ihn relevanten Infos
     public Wohnung(int zimmernummer){
         super(zimmernummer, 4, 150.00);
     }
     //
     //Methoden
+    //ruft die Zimmer print Methode auf
     public void print(){
         super.print();
         System.out.println("");
